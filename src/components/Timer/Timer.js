5e6e1d1f59b61eb4e952488c6009679d1649e7fe
jrios <servicios.jsr@gmail.com>
2022-02-25 08:18:47 -0700

    fix

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,40 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "assets/scss/timer.scss";
 
 export default function Timer() {
-  const [day, setDay] = useState(0);
-  const [hour, setHour] = useState(0);
-  const [minute, setMinute] = useState(0);
-  const [second, setSecond] = useState(0);
-  const countDate = new Date("Feb 25, 2022 00:00:00").getTime();
-  const now = new Date().getTime();
-  const gap = countDate - now;
-
-  //how the fuck does work time?
-  const SECOND = 1000;
-  const MINUTE = SECOND * 60;
-  const HOUR = MINUTE * 60;
-  const DAY = HOUR * 24;
-
-  const countdown = () => {
-    const textDay = Math.floor(gap / DAY);
-    const textHour = Math.floor((gap % DAY) / HOUR);
-    const textMinute = Math.floor((gap % HOUR) / MINUTE);
-    const textSecond = Math.floor((gap % MINUTE) / SECOND);
-
-    setDay(textDay);
-    setHour(textHour);
-    setMinute(textMinute);
-    setSecond(textSecond);
-  };
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      countdown();
-    }, 1000);
-    return () => clearTimeout(timer);
-  });
-
   return (
     <div className="container">
       <div className="timer">
